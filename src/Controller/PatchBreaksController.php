d5f640ef4f619075e64518de990db6888484245b
DenisDao57 <denis5710@hotmail.fr>
2020-01-01 12:39:01 +0100

    endpoints

--- src/Controller/PatchBreaksController.php
+++ src/Controller/PatchBreaksController.php
@@ -6,14 +6,15 @@ use DateTime;
 use App\Entity\Breaks;
 use App\Entity\Utilisateur;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityRepository;
 
-class PostBreaksController extends AbstractController
+class PatchBreaksController extends AbstractController
 {
     /**
-     * @Route("/api/breaks", name="post_breaks",methods={"post"})
+     * @Route("/api/breaks", name="patch_breaks",methods={"patch"})
      */
     public function __invoke():object
     {
@@ -33,23 +34,16 @@ class PostBreaksController extends AbstractController
             $sql="UPDATE breaks SET date_fin=NOW() WHERE utilisateur_id=1 AND date_fin IS NULL";
             $stmt=$entityManager->getConnection()->prepare($sql);
             $result=$stmt->execute();
+
+            $response = new Response();
+            $response->setStatusCode(200);
+            return $response;
         }
-
-
-        $sql="INSERT INTO breaks (date_debut,utilisateur_id) VALUES (NOW(),1)";
-        $stmt=$entityManager->getConnection()->prepare($sql);
-        $result=$stmt->execute();
-
-        $entities = $entityManager->getRepository(Breaks::class)->createQueryBuilder('o')
-        ->select('SEC_TO_TIME(SUM(TIME_TO_SEC(TIMEDIFF(o.date_fin,o.date_debut))))')
-        ->where('DAY(o.date_debut)=DAY(NOW())')
-        ->andwhere('MONTH(o.date_debut)=MONTH(NOW())')
-        ->andwhere('YEAR(o.date_debut)=YEAR(NOW())')
-        ->andwhere('o.utilisateur=1')
-        ->andwhere('o.date_fin > 0')
-        ->getQuery()
-        ->getArrayResult();
-
-        return new Jsonresponse($entities);   
+        else
+        {
+            $response = new Response();
+            $response->setStatusCode(500);
+            return $response;
+        }  
     }
 }
