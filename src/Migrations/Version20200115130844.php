fa5bb487eecdc7fe5a1891215217b54e12224a30
unknown <adtibau@gmail.com>
2020-01-15 14:10:57 +0100

    generate unique migration

--- src/Migrations/Version20200115130844.php
+++ src/Migrations/Version20200115130844.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200113113614 extends AbstractMigration
+final class Version20200115130844 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,11 +22,9 @@ final class Version20200113113614 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE breaks CHANGE date_fin date_fin DATETIME DEFAULT NULL, CHANGE utilisateur_id user_id INT NOT NULL');
+        $this->addSql('CREATE TABLE breaks (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, date_debut DATETIME NOT NULL, date_fin DATETIME DEFAULT NULL, INDEX IDX_7D048891A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE breaks ADD CONSTRAINT FK_7D048891A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
-        $this->addSql('CREATE INDEX IDX_7D048891A76ED395 ON breaks (user_id)');
-        $this->addSql('ALTER TABLE user ADD email VARCHAR(180) NOT NULL, ADD roles JSON NOT NULL, CHANGE ad_mail password VARCHAR(255) NOT NULL');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649E7927C74 ON user (email)');
     }
 
     public function down(Schema $schema) : void
@@ -35,9 +33,7 @@ final class Version20200113113614 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE breaks DROP FOREIGN KEY FK_7D048891A76ED395');
-        $this->addSql('DROP INDEX IDX_7D048891A76ED395 ON breaks');
-        $this->addSql('ALTER TABLE breaks CHANGE date_fin date_fin DATETIME NOT NULL, CHANGE user_id utilisateur_id INT NOT NULL');
-        $this->addSql('DROP INDEX UNIQ_8D93D649E7927C74 ON user');
-        $this->addSql('ALTER TABLE user DROP email, DROP roles, CHANGE password ad_mail VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('DROP TABLE breaks');
+        $this->addSql('DROP TABLE user');
     }
 }
